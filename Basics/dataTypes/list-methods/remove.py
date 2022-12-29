3d37d9efe45a43b4adc4ee157e4a4a1cc4cfb8c9
pavanprostack <pavanprostack@gmail.com>
2022-12-29 14:14:12 +0530

    remove

---
+++ Basics/dataTypes/list-methods/remove.py
@@ -0,0 +1,7 @@
+
+# it is used to remove element from list. based on specified value name.
+
+fruits=["apple", "mango", "kiwi", "banana"]
+
+fruits.remove("kiwi")  # It takes only one argument.
+print(fruits)
