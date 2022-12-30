91c5363483c0afb48a92653e66e87a4505a3f82f
pavanprostack <pavanprostack@gmail.com>
2022-12-30 22:18:08 +0530

    tuple

---
+++ dataTypes/Tuple-methods/one.py
@@ -0,0 +1,14 @@
+
+# Tuple inbuilt methods are two 1. index(), 2. count().
+
+# 1. index()  ==> it gives index value of given item.
+
+data=("apple", "mango", "orange")
+
+print(data.index("mango"))
+
+# 2. count()  ==> it gives no.of items present in the tuple.
+
+data1=("apple", "mango", "orange", "apple")
+
+print(data1.count("apple"))
---
+++ dataTypes/Tuple/one.js
@@ -0,0 +1,6 @@
+# Group of elements as one entity. duplicates are allowed. it is immutable. we can access elemets using index values.
+
+data=(1,2,3,4)
+
+print(data[2])
+
